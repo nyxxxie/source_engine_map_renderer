2626bb5088fe6a25a41a05c4540b2a0f13268dcd
Nyxxie <nyxxxxie@gmail.com>
2017-12-31 15:52:06 -0500

    Drew a rectangle!  This isn't useful towards drawing source engine maps, I'm just working through a tutorial right now.

--- src/main.c
+++ src/main.c
@@ -30,6 +30,21 @@
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 
+const char* vertex_shader_source =
+    "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "\n"
+    "void main() {\n"
+    "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\n";
+
+const char* fragment_shader_source =
+    "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main() {\n"
+    "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n" ;
+
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
@@ -69,21 +84,113 @@ int main(int argc, char* argv[]) {
     /*  Register a callback to resize the draw space when the window changes */
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    /* Create vertex shader */
+    unsigned int vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_source, NULL);
+    glCompileShader(vertex_shader);
+    int success;
+    char infolog[512];
+    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertex_shader, sizeof(infolog), NULL, infolog);
+    glDeleteShader(vertex_shader);
+        printf("Failed to compile vertex shader: \"%s\"\n", infolog);
+        return 1;
+    }
+    printf("Vertex shader compiled.\n");
+
+    /* Create fragment shader */
+    unsigned int fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_source, NULL);
+    glCompileShader(fragment_shader);
+    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(fragment_shader, sizeof(infolog), NULL, infolog);
+        glDeleteShader(fragment_shader);
+        printf("Failed to compile fragment shader: \"%s\"\n", infolog);
+        return 1;
+    }
+    printf("Fragment shader compiled.\n");
+
+    /* Create shader program */
+    unsigned int shader_program = glCreateProgram();
+    glAttachShader(shader_program, vertex_shader);
+    glAttachShader(shader_program, fragment_shader);
+    glLinkProgram(shader_program);
+    glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shader_program, sizeof(infolog), NULL, infolog);
+        glDeleteShader(fragment_shader);
+        glDeleteShader(vertex_shader);
+        printf("Failed to link shader program: \"%s\"\n", infolog);
+    }
+    printf("Shader program linked.\n");
+
+    /* Clean up shaders and tell opengl to use the shader program we just made */
+    glDeleteShader(fragment_shader);
+    glDeleteShader(vertex_shader);
+
+    /* Create objects */
+    unsigned int triangle_vbo, triangle_evbo, triangle_vao;
+    glGenVertexArrays(1, &triangle_vao);
+    glGenBuffers(1, &triangle_vbo);
+    glGenBuffers(1, &triangle_evbo);
+    glBindVertexArray(triangle_vao);  // All future binds will be associated with this vertex array
+
+    /* Copy verticies and to buffer object */
+    float vertices[] = {
+        0.5f,  0.5f, 0.0f,  // top right
+        0.5f, -0.5f, 0.0f,  // bottom right
+       -0.5f, -0.5f, 0.0f,  // bottom left
+       -0.5f,  0.5f, 0.0f   // top left 
+    };
+    glBindBuffer(GL_ARRAY_BUFFER, triangle_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    /* Copy indices and to buffer object */
+    unsigned int indices[] = {
+        0, 1, 3,  // first triangle
+        1, 2, 3   // second triangle
+    };
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, triangle_evbo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    /* Set vertex attributes */
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    /* Unbind objects */
+    glBindVertexArray(0);  // Unbind first so that we don't affect the vertex array with the following unbinds
+    glBindBuffer(GL_ARRAY_BUFFER, 0); 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0); 
+
+    /* Draw in wireframe mode */
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
     /* Start render loop! */
     printf("Rendering started.\n");
     while (!glfwWindowShouldClose(window)) {
         /* Process input */
         process_input(window);
 
-        /* Render stuff */
+        /* Set the color the screen will clear to and then clear it */
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Render triangle */
+        glUseProgram(shader_program);
+        glBindVertexArray(triangle_vao);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         /* Check and call events and swap the buffers */
         glfwPollEvents();
         glfwSwapBuffers(window);
     }
     printf("Render loop exited, closing program.\n");
+    glDeleteVertexArrays(1, &triangle_vao);
+    glDeleteBuffers(1, &triangle_vbo);
+    glDeleteBuffers(1, &triangle_evbo);
 
+    glfwTerminate();
     return 0;
 }
